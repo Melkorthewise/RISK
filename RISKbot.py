9f85697546c03ab1d7ff02771b71ea53ee3b6a4e
Melkorthewise <thomasdemunnik05@gmail.com>
2024-02-10 19:42:07 +0100

    Sync

--- RISKbot.py
+++ RISKbot.py
@@ -15,28 +15,45 @@ class RiskNet:
         plt.bar(self.continenten, self.aantal_landen, color="#ccd0c8")
 
         for y in self.landen[1]:
-            # print(y[1]["continent"])
-            # self.continent.append(y[1]["continent"])
-
             z = self.continenten.index(y[1]["continent"])
             self.lpc[z] += 1
 
-        # print(self.B)
-
         plt.bar(self.continenten, self.lpc)
-        plt.show()
+        # plt.show()
 
 
     def formule(self):
         self.score = 0
+        self.Continentbonus = continentbonus(self.landen)
 
         for x in self.landen[1]:
             self.A = len(x[1]["grenzen"])
-            self.C = continentbonus(self.landen)
 
             y = self.continenten.index(x[1]["continent"])
-            self.Continentbonus = self.C[y]
-                    
+            self.C = self.Continentbonus[y]
 
-            # score = self.A * self.w_A + self.C * self.w_C + self.S + self.w_S - self.B * self.w_B
+            aantal = self.aantal_landen[y]
+            print("aantal", aantal)
 
+            self.S = 0
+
+            # Kijken naar de omliggende landen naar hoeveel troepen daar staan van de tegenstanders
+            for a in x[1]["grenzen"]:
+                for y in range(len(self.kleuren_spelers)):
+                    for z in self.landen[y]:
+                        # print(z[0], x[0])
+                        if z[0] == a:
+                            self.S += z[1]["aantal_troepen"]
+
+            self.B = x[1]["aantal_troepen"]
+
+            print(self.A, self.C, self.S, self.B)
+
+            self.w_A = 1
+            self.w_C = 2
+            self.w_S = 3
+            self.w_B = 4
+
+            score = self.A * self.w_A + self.C * self.w_C + self.S + self.w_S - self.B * self.w_B
+
+            print(score)
